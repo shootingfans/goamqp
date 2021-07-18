65a17b8931a3e8d05688d8ce88cc09c49305a963
Jarod <xinghen_tj@qq.com>
2021-07-18 23:12:52 +0800

    增加注释

--- channel.go
+++ channel.go
@@ -4,9 +4,10 @@ import (
 	"github.com/streadway/amqp"
 )
 
+// Channel 定义通道
 type Channel struct {
 	*amqp.Channel
-	id    uint64
-	cid   uint64
-	state int32
+	id    uint64 // 通道唯一ID
+	cid   uint64 // 通道所属连接的ID
+	state int32  // 通道状态
 }
