21dfa0e49c52a6475a4204207f6d6feed1769686
Jarod <xinghen_tj@qq.com>
2021-07-27 23:43:14 +0800

    更新channel.go 增加两个方法 - Close 覆盖amqp.Close方法，增加对通道状态的切换 - IsClosed 获取目前通道是否关闭

--- channel.go
+++ channel.go
@@ -2,12 +2,27 @@ package goamqp
 
 import (
 	"github.com/streadway/amqp"
+	"sync/atomic"
 )
 
 // Channel 定义通道
 type Channel struct {
 	*amqp.Channel
-	id    uint64 // 通道唯一ID
-	cid   uint64 // 通道所属连接的ID
-	state int32  // 通道状态
+	id     uint64 // 通道唯一ID
+	cid    uint64 // 通道所属连接的ID
+	state  int32  // 通道状态
+	closed int32  // 是否关闭
+}
+
+// Close 关闭通道
+func (c *Channel) Close() error {
+	if !atomic.CompareAndSwapInt32(&c.closed, 0, 1) {
+		return nil
+	}
+	return c.Channel.Close()
+}
+
+// IsClosed 是否关闭了
+func (c *Channel) IsClosed() bool {
+	return atomic.LoadInt32(&c.closed) == 1
 }
